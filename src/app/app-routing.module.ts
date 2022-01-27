a7155df391e5342e2746c3e6603e25cf3d4347bb
unknown <shaunak12wankhade@ggmail.com>
2022-01-27 12:55:16 +0530

    [Shaunak] Added: modules to app.modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreatenotesComponent } from './Components/createnotes/createnotes.component';
 import { DashboardComponent } from './Components/dashboard/dashboard.component';
+import { DisplaynotesComponent } from './Components/displaynotes/displaynotes.component';
 import { ForgotemailComponent } from './Components/forgotemail/forgotemail.component';
 import { ForgotpasswordComponent } from './Components/forgotpassword/forgotpassword.component';
+import { GetallnotesComponent } from './Components/getallnotes/getallnotes.component';
+import { IconsComponent } from './Components/icons/icons.component';
 import { RegistrationComponent } from './Components/registration/registration.component';
 import { SigninComponent } from './Components/signin/signin.component';
 
@@ -10,8 +14,12 @@ const routes: Routes = [
   { path:'register',component:RegistrationComponent},
   { path:'signin',component:SigninComponent},
   { path:'forgotemail',component:ForgotemailComponent},
-  { path:'forgotpassword',component:ForgotpasswordComponent},
-  { path:'dashboard',component:DashboardComponent}
+  { path:'resetpassword/:token',component:ForgotpasswordComponent},
+  { path:'dashboard',component:DashboardComponent},
+  { path:'createnotes',component:CreatenotesComponent},
+  { path:'icons',component:IconsComponent},
+  { path:'getallnotes',component:GetallnotesComponent},
+  { path:'displaynotes',component:DisplaynotesComponent}
 ];
 
 @NgModule({
