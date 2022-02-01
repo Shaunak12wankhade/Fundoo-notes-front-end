839865db0a4307b0d5c4b7ef347213a91963337d
unknown <shaunak12wankhade@ggmail.com>
2022-02-01 23:12:35 +0530

    [Shaunak] Added: Fundoo notes modified icons component

--- src/app/Components/icons/icons.component.ts
+++ src/app/Components/icons/icons.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { NotesService } from 'src/app/services/notesservice/notes.service';
 
 @Component({
   selector: 'app-icons',
@@ -7,9 +8,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IconsComponent implements OnInit {
 
-  constructor() { }
+  @Input() notecard:any;
+ 
+  constructor(private note: NotesService) { }
 
   ngOnInit(): void {
   }
 
+  deletenote(){
+
+    let reqdata= {
+      
+      noteIdlist: [this.notecard.id],  //this notecard is coming from display.html - <app-icons & this noteIdlist is taken as a assumption by ourselves for taking id of notes
+      isDeleted: true,  // it is coming from backend 
+    }
+    this.note.userdeletenotes(reqdata).subscribe((response:any) =>{
+      console.log("Note is deleted");
+      
+      console.log(response)
+    })
+  }
+  
+  Archivenote(){
+
+    let reqdata={
+      
+      noteIdlist: [this.notecard.id],
+      isArchived:true,  // it is coming from backend api
+    }
+    this.note.userarchivenotes(reqdata).subscribe((response:any) =>{
+      console.log("Note is archived");
+
+      console.log(response);
+      
+      
+    })
+  }
 }
