7d1f0850e50f47932224a0b77ab3b095ee9958e2
unknown <shaunak12wankhade@ggmail.com>
2022-02-01 23:18:37 +0530

    [Shaunak] Added: Fundoo notes getservice method

--- src/app/services/httpservice/http.service.ts
+++ src/app/services/httpservice/http.service.ts
@@ -7,10 +7,15 @@ export class HttpService {
   baseurl= 'http://fundoonotes.incubation.bridgelabz.com/api/'
   constructor( private http :HttpClient) { }
 
-  postService(url:any, data:any, token:boolean=false, httpOptions:any){
+  postService(url:any, data:any, token:boolean=false, httpOptions:any){   // here data:any is used in context of whatever data we are sending to backend through post operation
    console.log(data)
    return this.http.post(this.baseurl+url, data, token && httpOptions)
   }
+
+  getService(url:any, token:boolean=false, httpOptions:any){   // no need to take data:any as we are not sending any data to backend rather we are getting data from backend
+    // console.log(data)
+    return this.http.get(this.baseurl+url,token && httpOptions)
+   }
 
   
 }
