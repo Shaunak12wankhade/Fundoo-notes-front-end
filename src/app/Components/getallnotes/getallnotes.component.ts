bdd2145c1667ab0ed12413e7be459c16647b3d3c
unknown <shaunak12wankhade@ggmail.com>
2022-02-01 23:11:16 +0530

    [Shaunak] Added: Fundoo notes modified get all notes component

--- src/app/Components/getallnotes/getallnotes.component.ts
+++ src/app/Components/getallnotes/getallnotes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NotesService } from 'src/app/services/notesservice/notes.service';
 
 @Component({
   selector: 'app-getallnotes',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./getallnotes.component.scss']
 })
 export class GetallnotesComponent implements OnInit {
+  notelist:any;  //  this notelist is called as parentmessage which is taken from getallnotes.html
 
-  constructor() { }
+
+  constructor( private note: NotesService) { }
 
   ngOnInit(): void {
+    this.getallnotes()  //we are calling this below method to see it in browser in our UI
+  }
+
+  getallnotes(){
+
+    this.note.usergetallnotes().subscribe((response:any) => {
+
+      console.log(response.data.data)   // .data.data is used because our notes in console is fetching from data inside data
+      this.notelist= response.data.data
+      this.notelist.reverse()
+
+    })
   }
 
 }
