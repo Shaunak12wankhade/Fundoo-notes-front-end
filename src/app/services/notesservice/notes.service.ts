e84530a426bc555cfbbf07104b527d4db2993507
unknown <shaunak12wankhade@ggmail.com>
2022-02-01 23:22:36 +0530

    [Shaunak] Added: Fundoo notes modified notes services

--- src/app/services/notesservice/notes.service.ts
+++ src/app/services/notesservice/notes.service.ts
@@ -8,7 +8,7 @@ import { HttpService } from '../httpservice/http.service';
 export class NotesService {
   token:any
   constructor( private httpService: HttpService ) {
-    this.token=localStorage.getItem("token")  // this token is taken from backend that is generated & stored in our local storage after we signin.  
+    this.token=localStorage.getItem("token")  // this token is taken from backend that is generated & stored in our local storage after we signin & we are setting this token in our signin.ts as localstorage.setitems  
    }
 
    usercreatenotes(data:any){
@@ -20,5 +20,50 @@ export class NotesService {
       })
     }
     return this.httpService.postService('notes/addNotes', data, true,header)  // here data written in () is coming from  my .ts file
+   }
+
+   usergetallnotes(){
+     
+    let header= {
+      headers: new HttpHeaders({
+        'Content-Type':'application/json',
+        'Authorization':this.token
+      })
+    }
+   return this.httpService.getService('notes/getNotesList', true,header )
+   }
+
+   userupdatenotes(data:any){
+
+    let header= {
+      headers: new HttpHeaders({
+        'Content-Type':'application/json',
+        'Authorization':this.token
+      })
+    }
+    return this.httpService.postService('notes/updateNotes',data,true,header)
+   }
+
+   userdeletenotes(data:any){
+
+    let header= {
+      headers: new HttpHeaders({
+        'Content-Type':'application/json',
+        'Authorization':this.token
+      })
+    }
+    return this.httpService.postService('notes/trashNotes',data,true,header)
+
+   }
+
+   userarchivenotes(data:any){
+
+    let header= {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization':this.token
+      })
+    }
+    return this.httpService.postService('notes/archiveNotes',data,true,header)
    }
 }
