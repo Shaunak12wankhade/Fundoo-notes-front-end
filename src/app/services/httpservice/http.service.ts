4b7abaa246ae0f87751260304130490592a3c48a
unknown <shaunak12wankhade@ggmail.com>
2022-02-09 08:43:10 +0530

    [Shaunak] Added: Fundo notes updated deployment project

--- src/app/services/httpservice/http.service.ts
+++ src/app/services/httpservice/http.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 @Injectable({
   providedIn: 'root'
 })
 export class HttpService {
-  baseurl= 'http://fundoonotes.incubation.bridgelabz.com/api/'
+  baseurl = environment.baseurl
   constructor( private http :HttpClient) { }
 
   postService(url:any, data:any, token:boolean=false, httpOptions:any){   // here data:any is used in context of whatever data we are sending to backend through post operation
