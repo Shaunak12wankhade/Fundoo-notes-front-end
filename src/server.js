d926185c96c5cc4c10471c98f459c5a3971c9c71
unknown <shaunak12wankhade@ggmail.com>
2022-03-05 14:33:52 +0530

    [Shaunak] Added: modified deploying of fundoo notes to heroku

--- src/server.js
+++ src/server.js
@@ -5,4 +5,5 @@ app.use(express.static(__dirname + '/dist/fundonotes'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
 '/dist/fundonotes/src/index.html'));});
-app.listen(process.env.PORT || 8080);
+// app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 3000,);
